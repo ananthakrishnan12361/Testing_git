9ca570ac1529b03f5cad2b77d60773496db1e13d
ananthakrishnan12361 <ananthakrishnan12361@gmail.com>
2022-09-19 17:09:25 +0530

    Added plases

---
+++ src/pages/Myname.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Myname() {
+  return (
+    <div>
+        <h1><center>Anantha krishnan S</center></h1>
+        <h1><center>Ayoor</center></h1>
+    </div>
+    
+  )
+}
